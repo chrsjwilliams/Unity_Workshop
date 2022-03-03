072efb3ff3bb04fea4cf42af8f231d1046b3c6a3
Chris Williams <chris@glowup.games>
2022-03-03 10:35:49 -0500

    Initial commit
    This has topics covered up until 02-17-2021:
    Counter app with upper and lower bounds
    - Creating a canvas
    - Creating buttons
    - Creating text on the canvas
    - Some scripting to change the text on screen and disable/enable buttons

---
+++ Unity Workshop/Assets/Scripts/Counter.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+/// <summary>
+/// This Counter class updates a text field and locks the displayed value between
+/// a min and a max value.
+///
+/// If you are opening this scene in a new Unity project, you will need
+/// to import images for your up button and your down button
+/// 
+/// </summary>
+
+public class Counter : MonoBehaviour
+{
+    public Button upButton;
+    public Button downButton;
+
+    [SerializeField] TextMeshProUGUI counter;
+    [SerializeField] int maxValue;
+    [SerializeField] int minValue;
+
+    int value = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Sets the text at the beginning of the application
+        counter.text = "Welcome! Press a button!";
+    }
+
+    public void UpdateCounter(int i)
+    {
+        // assigns value based on incoming input
+        value = value + i;
+
+        // Checks if we are larger than the max value
+        if(value > maxValue)
+        {
+            value = maxValue;
+            upButton.interactable = false;
+        }
+        else
+        {
+            // makes up button interactable if we are not at the max value
+            upButton.interactable = true;
+        }
+
+        // Checks if we are smaller than the min value
+        if(value < minValue)
+        {
+            value = minValue;
+            downButton.interactable = false;
+        }
+        else
+        {
+            // makes down button interactable if we are not smaller than the min value
+            downButton.interactable = true;
+        }
+
+        // Updates the text
+        if (value == 1)
+        {
+            counter.text = value + " click";
+        }
+        else
+        {
+            counter.text = value + " clicks";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
